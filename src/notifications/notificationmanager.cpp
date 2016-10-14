37bec7c6c93f161a8a44a2ee0b4a124fee69e567
Raine Makelainen <raine.makelainen@jolla.com>
2016-10-14 12:58:04 +0300

    [lipstick] Add privileged method that returns notification by category. Contributes to JB#36586

--- src/notifications/notificationmanager.cpp
+++ src/notifications/notificationmanager.cpp
@@ -473,6 +473,21 @@ NotificationList NotificationManager::GetNotifications(const QString &owner)
     return NotificationList(notificationList);
 }
 
+NotificationList NotificationManager::GetNotificationsByCategory(const QString &category)
+{
+    QList<LipstickNotification *> notificationList;
+    if (isPrivileged()) {
+        QHash<uint, LipstickNotification *>::const_iterator it = m_notifications.constBegin(), end = m_notifications.constEnd();
+        for ( ; it != end; ++it) {
+            LipstickNotification *notification = it.value();
+            if (notification->category() == category) {
+                notificationList.append(notification);
+            }
+        }
+    }
+    return NotificationList(notificationList);
+}
+
 uint NotificationManager::nextAvailableNotificationID()
 {
     bool idIncreased = false;
@@ -1010,6 +1025,22 @@ uint NotificationManager::callerProcessId() const
     } else {
         return QCoreApplication::applicationPid();
     }
+}
+
+bool NotificationManager::isPrivileged() const
+{
+    uint pid = callerProcessId();
+    QFileInfo info(QString("/proc/%1").arg(pid));
+    if (info.group() != QLatin1String("privileged") && info.owner() != QLatin1String("root")) {
+        QString errorString = QString("PID %1 is not in privileged group").arg(pid);
+        if (calledFromDBus()) {
+            sendErrorReply(QDBusError::AccessDenied, errorString);
+        } else {
+            qWarning() << errorString;
+        }
+        return false;
+    }
+    return true;
 }
 
 void NotificationManager::invokeAction(const QString &action)
