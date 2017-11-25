c4e8d7fd4159c514bab38b092b44b74886a3e7ee
Florent Revest <revestflo@gmail.com>
2017-11-25 10:05:56 +0100

    Add a default english translation

---
+++ i18n/lipstick-en_GB.ts
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="qtn_ap_lipstick">
+        <location filename="../src/homeapplication.cpp" line="80"/>
+        <source>System</source>
+        <translation>System</translation>
+    </message>
+    <message id="qtn_ener_charging">
+        <location filename="../src/notifications/batterynotifier.cpp" line="212"/>
+        <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="142"/>
+        <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="166"/>
+        <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="181"/>
+        <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="258"/>
+        <source>Charging</source>
+        <translation>Charging</translation>
+    </message>
+    <message id="qtn_ener_charcomp">
+        <location filename="../src/notifications/batterynotifier.cpp" line="216"/>
+        <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="114"/>
+        <source>Charging complete</source>
+        <translation>Charging complete</translation>
+    </message>
+    <message id="qtn_ener_remcha">
+        <location filename="../src/notifications/batterynotifier.cpp" line="220"/>
+        <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="172"/>
+        <source>Disconnect charger from power supply to save energy</source>
+        <translation>Disconnect charger from power supply to save energy</translation>
+    </message>
+    <message id="qtn_ener_repcharger">
+        <location filename="../src/notifications/batterynotifier.cpp" line="224"/>
+        <source>Charging not started. Replace charger.</source>
+        <translation>Charging not started. Replace charger.</translation>
+    </message>
+    <message id="qtn_ener_rebatt">
+        <location filename="../src/notifications/batterynotifier.cpp" line="228"/>
+        <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="129"/>
+        <source>Recharge battery</source>
+        <translation>Recharge battery</translation>
+    </message>
+    <message id="qtn_ener_ent_psnote">
+        <location filename="../src/notifications/batterynotifier.cpp" line="232"/>
+        <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="205"/>
+        <source>Entering power save mode</source>
+        <translation>Entering power save mode</translation>
+    </message>
+    <message id="qtn_ener_exit_psnote">
+        <location filename="../src/notifications/batterynotifier.cpp" line="236"/>
+        <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="213"/>
+        <source>Exiting power save mode</source>
+        <translation>Exiting power save mode</translation>
+    </message>
+    <message id="qtn_ener_lowbatt">
+        <location filename="../src/notifications/batterynotifier.cpp" line="240"/>
+        <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="101"/>
+        <source>Low battery</source>
+        <translation>Low battery</translation>
+    </message>
+    <message id="qtn_ener_nopowcharge">
+        <location filename="../src/notifications/batterynotifier.cpp" line="244"/>
+        <source>Not enough power to charge</source>
+        <translation>Not enough power to charge</translation>
+    </message>
+    <message id="qtn_memu_memlow_notification_src">
+        <location filename="../src/notifications/diskspacenotifier.cpp" line="56"/>
+        <source>Getting low with storage. Please check.</source>
+        <translation>Getting low with storage. Please check.</translation>
+    </message>
+    <message id="qtn_shut_high_temp_warning">
+        <location filename="../src/notifications/thermalnotifier.cpp" line="34"/>
+        <location filename="../tests/ut_thermalnotifier/ut_thermalnotifier.cpp" line="78"/>
+        <source>Device is getting hot. Close all apps.</source>
+        <translation>Device is getting hot. Close all apps.</translation>
+    </message>
+    <message id="qtn_shut_high_temp_alert">
+        <location filename="../src/notifications/thermalnotifier.cpp" line="38"/>
+        <location filename="../tests/ut_thermalnotifier/ut_thermalnotifier.cpp" line="84"/>
+        <source>Device is overheating. turn it off.</source>
+        <translation>Device is overheating. turn it off.</translation>
+    </message>
+    <message id="qtn_shut_low_temp_warning">
+        <location filename="../src/notifications/thermalnotifier.cpp" line="42"/>
+        <location filename="../tests/ut_thermalnotifier/ut_thermalnotifier.cpp" line="90"/>
+        <source>Low temperature warning</source>
+        <translation>Low temperature warning</translation>
+    </message>
+    <message id="qtn_shut_high_temp">
+        <location filename="../src/shutdownscreen.cpp" line="79"/>
+        <location filename="../tests/ut_shutdownscreen/ut_shutdownscreen.cpp" line="101"/>
+        <source>Temperature too high. Device shutting down.</source>
+        <translation>Temperature too high. Device shutting down.</translation>
+    </message>
+    <message id="qtn_shut_unplug_usb">
+        <location filename="../src/shutdownscreen.cpp" line="84"/>
+        <location filename="../tests/ut_shutdownscreen/ut_shutdownscreen.cpp" line="108"/>
+        <source>USB cable plugged in. Unplug the USB cable to shutdown.</source>
+        <translation>USB cable plugged in. Unplug the USB cable to shutdown.</translation>
+    </message>
+    <message id="qtn_shut_batt_empty">
+        <location filename="../src/shutdownscreen.cpp" line="89"/>
+        <location filename="../tests/ut_shutdownscreen/ut_shutdownscreen.cpp" line="115"/>
+        <source>Battery empty. Device shutting down.</source>
+        <translation>Battery empty. Device shutting down.</translation>
+    </message>
+    <message id="qtn_usb_device_locked">
+        <location filename="../src/usbmodeselector.cpp" line="103"/>
+        <source>Unlock device first</source>
+        <translation>Unlock device first</translation>
+    </message>
+    <message id="qtn_usb_disconnected">
+        <location filename="../src/usbmodeselector.cpp" line="127"/>
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="180"/>
+        <source>USB cable disconnected</source>
+        <translation>USB cable disconnected</translation>
+    </message>
+    <message id="qtn_usb_connection_sharing_active">
+        <location filename="../src/usbmodeselector.cpp" line="132"/>
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="189"/>
+        <source>USB tethering in use</source>
+        <translation>USB tethering in use</translation>
+    </message>
+    <message id="qtn_usb_mtp_active">
+        <location filename="../src/usbmodeselector.cpp" line="135"/>
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="185"/>
+        <source>MTP mode in use</source>
+        <translation>MTP mode in use</translation>
+    </message>
+    <message id="qtn_usb_storage_active">
+        <location filename="../src/usbmodeselector.cpp" line="138"/>
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="183"/>
+        <source>Mass storage in use</source>
+        <translation>Mass storage in use</translation>
+    </message>
+    <message id="qtn_usb_sdk_active">
+        <location filename="../src/usbmodeselector.cpp" line="141"/>
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="184"/>
+        <source>SDK mode in use</source>
+        <translation>SDK mode in use</translation>
+    </message>
+    <message id="qtn_usb_sync_active">
+        <location filename="../src/usbmodeselector.cpp" line="144"/>
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="181"/>
+        <source>Sync and connect in use</source>
+        <translation>Sync and connect in use</translation>
+    </message>
+    <message id="qtn_usb_adb_active">
+        <location filename="../src/usbmodeselector.cpp" line="147"/>
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="186"/>
+        <source>Adb mode in use</source>
+        <translation>Adb mode in use</translation>
+    </message>
+    <message id="qtn_usb_diag_active">
+        <location filename="../src/usbmodeselector.cpp" line="150"/>
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="187"/>
+        <source>Diag mode in use</source>
+        <translation>Diag mode in use</translation>
+    </message>
+    <message id="qtn_usb_host_mode_active">
+        <location filename="../src/usbmodeselector.cpp" line="153"/>
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="188"/>
+        <source>USB switched to host mode (OTG)</source>
+        <translation>USB switched to host mode (OTG)</translation>
+    </message>
+    <message id="qtn_usb_filessystem_inuse">
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="233"/>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_usb_mount_failed">
+        <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="237"/>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
