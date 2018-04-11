4510fb487eecd8f77942e3e8f713bd7e999952c0
Pavel Borecki <pavel.borecki@gmail.com>
2018-04-11 04:20:29 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (27 of 27 strings)
    
    Translation: AsteroidOS/Lipstick
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/lipstick/cs/

--- i18n/lipstick-cs.ts
+++ i18n/lipstick-cs.ts
@@ -33,7 +33,7 @@
         <location filename="../src/notifications/batterynotifier.cpp" line="224"/>
         <source>Not charging. Adjust or recycle charger.</source>
         <oldsource>Charging not started. Replace charger.</oldsource>
-        <translation type="unfinished">Nabíjení nezačalo. Vyměňte nabíječku.</translation>
+        <translation>Nabíjení nezačalo. Seřiďte nebo zrecyklujte nabíječku.</translation>
     </message>
     <message id="qtn_ener_rebatt">
         <location filename="../src/notifications/batterynotifier.cpp" line="228"/>
@@ -58,40 +58,40 @@
         <location filename="../tests/ut_batterynotifier/ut_batterynotifier.cpp" line="101"/>
         <source>Low battery level</source>
         <oldsource>Low battery</oldsource>
-        <translation type="unfinished">Nízký stav nabití akumulátoru</translation>
+        <translation>Nízký stav nabití aku</translation>
     </message>
     <message id="qtn_ener_nopowcharge">
         <location filename="../src/notifications/batterynotifier.cpp" line="244"/>
         <source>Power input below charging threshold</source>
         <oldsource>Not enough power to charge</oldsource>
-        <translation type="unfinished">Nedostatek energie pro nabíjení</translation>
+        <translation>Napájení nedosahuje úrovně potřebné pro nabíjení</translation>
     </message>
     <message id="qtn_memu_memlow_notification_src">
         <location filename="../src/notifications/diskspacenotifier.cpp" line="56"/>
         <source>Storage space running out.</source>
         <oldsource>Getting low with storage. Please check.</oldsource>
-        <translation type="unfinished">Dochází místo na úložišti. Podívejte se na to.</translation>
+        <translation>Dochází prostor na úložišti.</translation>
     </message>
     <message id="qtn_shut_high_temp_warning">
         <location filename="../src/notifications/thermalnotifier.cpp" line="34"/>
         <location filename="../tests/ut_thermalnotifier/ut_thermalnotifier.cpp" line="78"/>
         <source>Device getting hot. Close all apps.</source>
         <oldsource>Device is getting hot. Close all apps.</oldsource>
-        <translation type="unfinished">Zařízení se přehřívá. Zavřete všechny aplikace.</translation>
+        <translation>Zařízení se přehřívá. Ukončete všechny aplikace.</translation>
     </message>
     <message id="qtn_shut_high_temp_alert">
         <location filename="../src/notifications/thermalnotifier.cpp" line="38"/>
         <location filename="../tests/ut_thermalnotifier/ut_thermalnotifier.cpp" line="84"/>
         <source>Device overheating. Turn it off.</source>
         <oldsource>Device is overheating. Turn it off.</oldsource>
-        <translation type="unfinished">Zařízení se přehřívá. Vypněte ho.</translation>
+        <translation>Zařízení se přehřívá. Vypněte ho.</translation>
     </message>
     <message id="qtn_shut_low_temp_warning">
         <location filename="../src/notifications/thermalnotifier.cpp" line="42"/>
         <location filename="../tests/ut_thermalnotifier/ut_thermalnotifier.cpp" line="90"/>
         <source>The device is too cold</source>
         <oldsource>Low temperature warning</oldsource>
-        <translation type="unfinished">Varování o nízké teplotě</translation>
+        <translation>Zařízení je příliš prochladlé</translation>
     </message>
     <message id="qtn_shut_high_temp">
         <location filename="../src/shutdownscreen.cpp" line="79"/>
@@ -104,7 +104,7 @@
         <location filename="../tests/ut_shutdownscreen/ut_shutdownscreen.cpp" line="108"/>
         <source>USB cable plugged in. Unplug it to shut down device.</source>
         <oldsource>USB cable plugged in. Unplug the USB cable to shutdown.</oldsource>
-        <translation type="unfinished">Je připojený USB kabel. Pro vypnutí ho odpojte.</translation>
+        <translation>Je připojen USB kabel. Pro vypnutí zařízení ho odpojte.</translation>
     </message>
     <message id="qtn_shut_batt_empty">
         <location filename="../src/shutdownscreen.cpp" line="89"/>
@@ -152,7 +152,7 @@
         <location filename="../tests/ut_usbmodeselector/ut_usbmodeselector.cpp" line="181"/>
         <source>Sync-and-connect in use</source>
         <oldsource>Sync and connect in use</oldsource>
-        <translation type="unfinished">Je používána synchronizace a spojení</translation>
+        <translation>Je používána funkce sync-and-connect</translation>
     </message>
     <message id="qtn_usb_adb_active">
         <location filename="../src/usbmodeselector.cpp" line="147"/>
