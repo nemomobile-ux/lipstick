3fb6ee78e35be1ec0e70dbb730c3b07ace4a164d
Chupligin Sergey <neochapay@gmail.com>
2024-05-05 16:26:53 +0000

    [BluetoothObexAgent] Show obex dialog as homewindow

--- src/bluetooth/bluetoothobexagent.h
+++ src/bluetooth/bluetoothobexagent.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Chupligin Sergey <neochapay@gmail.com>
+ * Copyright (C) 2021-2024 Chupligin Sergey <neochapay@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -28,8 +28,13 @@
 
 #include "lipstickglobal.h"
 
+class HomeWindow;
+
 class LIPSTICK_EXPORT BluetoothObexAgent : public BluezQt::ObexAgent {
     Q_OBJECT
+    Q_PROPERTY(bool windowVisible READ windowVisible WRITE setWindowVisible NOTIFY windowVisibleChanged)
+    Q_PROPERTY(QString deviceName READ deviceName NOTIFY transferChanged)
+    Q_PROPERTY(QString fileName READ fileName NOTIFY transferChanged)
 
 public:
     explicit BluetoothObexAgent(QObject* parent = nullptr);
@@ -38,23 +43,38 @@ public:
     QDBusObjectPath objectPath() const override;
     void authorizePush(BluezQt::ObexTransferPtr transfer, BluezQt::ObexSessionPtr session, const BluezQt::Request<QString>& request) override;
 
+    bool windowVisible() const;
+    void setWindowVisible(bool visible);
+
+    QString deviceName() const;
+    QString fileName() const;
+
 signals:
-    void showRequiesDialog(const QString deviceName, const QString fileName);
+    void showRequiesDialog();
     void requestConfirmationAccept();
     void requestConfirmationReject();
     void transferError();
     void transferFinished(QString resultPath);
+
+    void windowVisibleChanged();
+    void transferChanged();
 
 private slots:
     void startServiceFinished(BluezQt::PendingCall *call);
     void obexManagerStartResult(BluezQt::InitObexManagerJob *job);
     void obexDataTransferFinished(BluezQt::ObexTransfer::Status status);
 
+    void createWindow();
+
 private:
+    HomeWindow *m_requestODialogWindow;
+
     BluezQt::ObexManager *m_obexManager;
     QString m_temporaryPath;
     QString m_transferName;
 
+    QString m_deviceName;
+    QString m_fileName;
 };
 
 #endif // BLUETOOTHOBEXAGENT_H
